f7543c3d985b18ba48c68304e801993c1e0f83c7
Tiago Mesquita <tiagomac@gmail.com>
2021-02-01 22:35:28 -0300

    adicionando paginacao

--- gerenciador-de-tarefas/src/listar/itens-lista-tarefas.js
+++ gerenciador-de-tarefas/src/listar/itens-lista-tarefas.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faEdit} from "@fortawesome/free-solid-svg-icons";
-import {A} from 'hookrouter';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { A } from 'hookrouter';
 import ConcluirTarefa from './concluir-tarefa';
-import RemoverTarefa from "./remover.tarefa";
 
 function ItensListaTarefas(props) {
 
@@ -17,27 +16,23 @@ function ItensListaTarefas(props) {
             <tr key={tarefa.id} data-testid="tarefa">
                 <td width="75%"
                     data-testid="nome-tarefa"
-                    style={{textDecoration: marcarConcluida(tarefa)}}>
+                    style={{ textDecoration: marcarConcluida(tarefa) }}>
                     {tarefa.nome}
                 </td>
                 <td className="text-right">
                     <ConcluirTarefa
                         tarefa={tarefa}
                         recarregarTarefas={props.recarregarTarefas}
-                        className={tarefa.concluida ? 'hidden' : null}/>
+                        className={tarefa.concluida ? 'hidden' : null } />
                     &nbsp;
                     <A href={"/atualizar/" + tarefa.id}
                        className={tarefa.concluida ? 'hidden' : 'btn btn-warning btn-sm'}>
-                        <FontAwesomeIcon icon={faEdit}></FontAwesomeIcon>
+                        <FontAwesomeIcon icon={faEdit} />
                     </A>
-                    &nbsp;
-                    <RemoverTarefa tarefa={tarefa} recarregarTarefas={props.recarregarTarefas}/>
-
                 </td>
             </tr>
         )
     );
-
 }
 
 ItensListaTarefas.propTypes = {
--- gerenciador-de-tarefas/src/listar/listar-tarefas.js
+++ gerenciador-de-tarefas/src/listar/listar-tarefas.js
@@ -4,27 +4,34 @@ import {Table} from 'react-bootstrap';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faPlus} from '@fortawesome/free-solid-svg-icons';
 import ItensListaTarefas from "./itens-lista-tarefas";
+import Paginacao from './paginacao';
 
 function ListarTarefas() {
 
+    const ITENS_POR_PAG = 3;
+
     const [tarefas, setTarefas] = useState([]);
     const [carregarTarefas, setCarregarTarefas] = useState(true);
+    const [totalItems, setTotalItems] = useState(0);
+    const [paginaAtual, setPaginaAtual] = useState(1);
 
     useEffect(() => {
-
         function obterTarefas() {
             const tarefasDb = localStorage['tarefas'];
-            let listarTarefas = tarefasDb ? JSON.parse(tarefasDb) : [];
-            setTarefas(listarTarefas);
-            console.log(listarTarefas);
+            let listaTarefas = tarefasDb ? JSON.parse(tarefasDb) : [];
+            setTotalItems(listaTarefas.length);
+            setTarefas(listaTarefas.splice(paginaAtual - 1) * ITENS_POR_PAG, ITENS_POR_PAG);
         }
-
         if (carregarTarefas) {
             obterTarefas();
             setCarregarTarefas(false);
         }
+    }, [carregarTarefas, paginaAtual]);
 
-    }, [carregarTarefas]);
+    function handleMudarPagina(pagina) {
+        setPaginaAtual(pagina);
+        setCarregarTarefas(true);
+    }
 
     return (
         <div className='text-center'>
@@ -51,6 +58,11 @@ function ListarTarefas() {
                 />
                 </tbody>
             </Table>
+            <Paginacao
+                totalItems={totalItems}
+                itemsPorPagina={ITENS_POR_PAG}
+                paginaAtual={paginaAtual}
+                mudarPagina={handleMudarPagina} />
         </div>
     );
 }
